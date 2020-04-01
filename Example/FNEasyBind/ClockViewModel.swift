01fe74068bc47cc7b04838afdf78f555f6910df7
Fabio Nisci <fabionisci@gmail.com>
2020-04-01 23:35:11 +0200

    fixed double call of subscribe block when not needed

--- Example/FNEasyBind/ClockViewModel.swift
+++ Example/FNEasyBind/ClockViewModel.swift
@@ -13,16 +13,19 @@ class ClockViewModel {
 
     /// As this is a public property the value is immutable, so you can only subscribe to changes.
     var formattedDateTime: Observable<String> {
-        formattedDateTimeVariable
+        formattedDateTimeVariable.asObservable()
     }
     
     /// As this is our private property the value is mutable, so only this class can modify it.
-    private let formattedDateTimeVariable = Variable(ClockViewModel.formattedDate())
+    private let formattedDateTimeVariable = Variable<String>(ClockViewModel.formattedDate())
+//    private let formattedDateTimeVariable = PublishSubject<String>()
     
     private var timer: Timer?
     init() {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
-            self?.formattedDateTimeVariable.value = ClockViewModel.formattedDate()
+            let formatted = ClockViewModel.formattedDate()
+            print("assigning: \(formatted)")
+            self?.formattedDateTimeVariable.onNext(value: formatted)
         })
     }
     
--- FNEasyBind/Classes/Observable.swift
+++ FNEasyBind/Classes/Observable.swift
@@ -25,14 +25,11 @@ public class Observable<T> {
     
     fileprivate var _onDispose: () -> Void
     
-    public init(_ value: T, onDispose: @escaping () -> Void = {}) {
-        _value = value
+    /// Create a new Observable (use a subclass, this is abstract)
+    /// - Note: all subclass must declare the init as `public`, you should not use this init
+    /// - Parameter onDispose: optional function to call on dispose
+    public init(onDispose: @escaping () -> Void = {}) {
         _onDispose = onDispose
-    }
-    
-    public init(_ value: T) {
-        _value = value
-        _onDispose = {}
     }
     
     /// When you use the .subscribe method, you are subscribing an event handler to an observable sequence. In the closure, you specify how you want to handle the different events that are emitted by the sequence. In this case, regardless of the event type, we are printing the event to the console.
@@ -47,13 +44,12 @@ public class Observable<T> {
     ///   - observer: subscribed observer
     /// - Returns: A Disposable object that can be added to the (you guessed it) dispose bag. The .diposed(by: &disposeBag) operator, simply adds the subscription (of Disposable type) to the dispose bag
     public func subscribe(_ queue: DispatchQueue? = nil, _ observer: @escaping Observer) -> Disposable {
+        
         lock.lock()
         defer { lock.unlock() }
         
         let id = uniqueID.next()!
-        
         observers[id] = (observer, queue)
-        notify(observer: observer, queue: queue, value: value)
         
         let disposable = Disposable { [weak self] in
             self?.observers[id] = nil
@@ -84,23 +80,36 @@ public class Observable<T> {
     }
     
     
-    fileprivate var _value: Value{
-        didSet {
-            let newValue = _value
-            notifyObservers(value: newValue, oldValue: oldValue)
-        }
+    /// Current value
+    /// - Note: Subclasses need to overwrite and implement this computed property.
+    /// - Warning: Always use class lock and unlock on defer{} on set to avoid race conditions
+    public var value: Value? {
+        fatalError("Subclasses need to overwrite and implement this computed property.")
     }
     
+    
+//    fileprivate var _value: Value
+//    {
+//        didSet {
+//            let newValue = _value
+//            notifyObservers(value: newValue, oldValue: oldValue)
+//        }
+//    }
+//
     /// Use this only for testing, you should get the right value using *-subscribe* method
-    public var value: Value {
-        get {
-            return _value
-        }
-        set {
-            lock.lock()
-            defer { lock.unlock() }
-            _value = newValue
-        }
+//    public var value: Value {
+//        get {
+//            return _value
+//        }
+//        set {
+//            lock.lock()
+//            defer { lock.unlock() }
+//            _value = newValue
+//        }
+//    }
+    
+    public func onNext(value:T){
+        
     }
     
 }
@@ -112,6 +121,22 @@ public class Observable<T> {
 /// A good use case for a PublishSubject is any sort of ticker. If you want a live feed of information, you likely don’t want to receive any historical events. This makes the PublishSubject ideal for your purpose.
 public final class PublishSubject<T>: Observable<T>{
     
+    private var _value: Value?
+    public override var value: Value? {
+        get {
+            return _value
+        }
+        set {
+            lock.lock()
+            defer { lock.unlock() }
+            _value = newValue
+        }
+    }
+    
+    public override init(onDispose: @escaping () -> Void = {}) {
+        super.init(onDispose: onDispose)
+    }
+    
     /// Updates the publish subject using the given value.
     public func update(_ value: Value) {
         let oldValue = _value
@@ -122,41 +147,55 @@ public final class PublishSubject<T>: Observable<T>{
         notifyObservers(value: value, oldValue: oldValue)
     }
     
+    public override func onNext(value:T){
+        self.update(value)
+    }
+    
 }
 
 // MARK: BehaviorSubject
-/// **BehaviorSubject - replay a single next event (last)**
+/// **BehaviorSubject - replay a single next() event (last)**
 /// A BehaviorSubject stores the most recent next() event, which is able to be replayed to new subscribers. In other words, a new subscriber can receive the most recent next() event even if they subscribe after the event was emitted. A BehaviorSubject must not have an empty buffer, so it is initialized with a starting value which acts as the initial next() event. This value gets overwritten as soon as a new element is added to the sequence.
 public final class BehaviorSubject<T>: Variable<T> {
     
 }
 
 // MARK: Variable
-/// **Variable - replay a single next event (last)**
+/// **Variable - replay a single next() event (last)**
 /// At it’s heart, Variable is a wrapper around BehaviorSubject that allows for simpler handling. Instead of the usual sending of next() events, Variable provides dot syntax for getting and setting a single value that is both emitted as a next() event and stored for replay. The exposed .value property gets and sets the value to a privately stored property _value. Additionally, it creates a new next() event for the privately held BehaviorSubject contained in the Variable in the setter method. Variable also has a method .asObservable()which returns the privately held BehaviorSubject in order to manage its subscribers.
 public class Variable<T>: Observable<T> {
+    
+    fileprivate var _value: Value
+    public override var value: Value {
+        get {
+            return _value
+        }
+        set {
+            lock.lock()
+            defer { lock.unlock() }
+            _value = newValue
+        }
+    }
 
     public func asObservable() -> Variable<T> {
         return self
     }
 
     /// Initializes a new variable with the given value.
-    ///
-    /// - Note: We keep the initializer to the super class `Observable` fileprivate in order to verify always having a value.
-    public override convenience init(_ value: Value) {
-        self.init(value, onDispose: {})
+    public init(_ value: Value, onDispose: @escaping () -> Void = {}) {
+        _value = value
+        super.init(onDispose: onDispose)
     }
     
-    public override init(_ value: T, onDispose: @escaping () -> Void = {}) {
-        super.init(value, onDispose: onDispose)
-        _value = value
-    }
-
     public override func subscribe(_ queue: DispatchQueue? = nil, _ observer: @escaping Observable<T>.Observer) -> Disposable {
         // A variable should inform the observer with the initial value.
-        observer(_value, nil)
+        observer(_value, nil) //newValue, oldValue
 
         return super.subscribe(queue, observer)
+    }
+    
+    public override func onNext(value:T){
+        self.value = value
     }
 }
 
@@ -167,10 +206,8 @@ public class Variable<T>: Observable<T> {
 /// Good use cases for a Replay subject are those recording historical information such as storing most recent search terms or operations that a user might want to undo.
 @available(*, unavailable)
 public final class ReplaySubject<T>: Observable<T>{
-    override convenience init(_ value: T) {
-        self.init(value, onDispose: {})
-    }
-    override init(_ value: T, onDispose: @escaping () -> Void = {}) {
+    public init(_ value: T?, bufferSize:Int, onDispose: @escaping () -> Void = {}) {
+        super.init(onDispose: {})
         fatalError("ReplaySubject to be implemented")
     }
 }
